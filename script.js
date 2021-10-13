d62124223daa3e25c6708d3102d1e0cb0e988637
gökalp <avcu219@gmail.com>
2021-10-14 00:59:55 +0300

    add js,cs

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ let inputText = document.getElementById("inputText");
 
 addToDoButton.addEventListener("click", function(){
     let paragraph = document.createElement("p"); // paragrafı oluştur
-    paragraph.classList.add("");
+    paragraph.classList.add(".paragraph-styling");
     toDoContainer.appendChild(paragraph); // paragrafı div'in içerisine at
     paragraph.innerHTML = inputText.value; 
     inputText.value = ""; // yukarıdaki input text'in içeriğini sıfırla
 
+    paragraph.addEventListener("click", function(){
+      paragraph.style.textDecoration = "line-through";
+    })
 })
