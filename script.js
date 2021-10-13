a0e94300f7815942c646af263a44b8cddd06dc0c
gökalp <avcu219@gmail.com>
2021-10-14 00:53:05 +0300

    add js, cs

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let addToDoButton = document.getElementById("addToDo");
 let toDoContainer = document.getElementById("toDoContainer");
+let inputText = document.getElementById("inputText");
 
 addToDoButton.addEventListener("click", function(){
-    let paragraph = document.createElement("p");
-    toDoContainer.appendChild(paragraph);
+    let paragraph = document.createElement("p"); // paragrafı oluştur
+    paragraph.classList.add("");
+    toDoContainer.appendChild(paragraph); // paragrafı div'in içerisine at
+    paragraph.innerHTML = inputText.value; 
+    inputText.value = ""; // yukarıdaki input text'in içeriğini sıfırla
 
 })
